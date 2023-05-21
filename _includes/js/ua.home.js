f3ab9353c51671a603cb1b875f651989db70c7cc
Vasyl Mosiychuk <134104152+reuzua@users.noreply.github.com>
2023-05-21 18:33:47 +0300

    Change #13

--- _includes/js/ua.home.js
+++ _includes/js/ua.home.js
@@ -51,7 +51,7 @@
     $(document).ready(function() { {{ region | remove: "-" }}Random(); });
   {%- else -%}
     {%- if sr.url != site.url -%}
-      document.getElementById("{{ region }}").innerHTML = '<div class="alert alert-success mb-0" role="alert"><a href="{{ sr.url }}" class="alert-link">Додати&nbsp;оголошення</a> про {{ sr.title | replace_first: "Н", "н" }}</div>';
+      document.getElementById("{{ region }}").innerHTML = '<div class="alert alert-success mb-0" role="alert"> <a href="{{ sr.url }}" class="alert-link">Додати&nbsp;оголошення</a> про {{ sr.title | replace_first: 'Н', 'н' }}</div>';
     {%- endif -%}
   {%- endif -%}
 {%- endfor -%}
